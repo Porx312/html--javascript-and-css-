8742755a01e72f834f34650acaa0588130355b2a
porx <105926826+Porx312@users.noreply.github.com>
2023-07-01 19:48:49 +0200

    async

---
+++ Day 3/javascript 3/main.js
@@ -0,0 +1,20 @@
+/* 
+let contador = 0
+
+let inter = setInterval(() => {
+  contador++
+  console.log(contador)
+},1000);
+ */
+
+
+const eventoFuturo = (res)=>{
+return new Promise((resolve,reject)=>{
+  setTimeout(() => {
+  res === true ? resolve("verdadero") : reject("faslso")
+  })
+},2000)
+}
+const valor = true
+console.log(eventoFuturo(valor))
+
