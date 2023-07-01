66d6a614f6af684833f5b404b8ea66e39af658a1
porx <105926826+Porx312@users.noreply.github.com>
2023-07-01 20:44:11 +0200

    librery

--- Day 3/javascript 3/librerias.js
+++ Day 3/javascript 3/librerias.js
@@ -1,6 +1,41 @@
-Swal.fire({
-  title: 'success!',
-  text: 'Do you want to continues',
-  icon:'success',
-  confirmButtonText: 'yes'
+const sweatBtn = document.getElementById('sweatAlert')
+
+sweatBtn.addEventListener('click', (e)=>{
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+      )
+    }
+  })
+})
+
+
+const tostybtn = document.querySelector('#toastify')
+
+tostybtn.addEventListener('click', () =>{
+  Toastify({
+    text: "This is a toast",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
 })
