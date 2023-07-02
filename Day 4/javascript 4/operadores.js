c8333ec46e9adf073eeb6313c89fc095ae60718e
porx <105926826+Porx312@users.noreply.github.com>
2023-07-02 22:09:55 +0200

    operadores

--- Day 4/javascript 4/operadores.js
+++ Day 4/javascript 4/operadores.js
@@ -11,11 +11,35 @@ numero += 2
 
 numero === 1 ? console.log("el numero es 1") : console.log("el numero es 2") */
 
-
 /* let edad = 15;
 
  edad >= 18? console.log("ingresa") : console.log("no ingresas")
 
  */
-let edad = 20
-console.log(edad >=18 ? "puedes ingresar" : "no ingres")
+/* let edad = 20
+console.log(edad >=18 ? "puedes ingresar" : "no ingres") */
+
+//  ??
+
+// let numero = 0; 
+/* 
+nullizs,null,undefined */
+// console.log(numero ?? "el numero no es valido");
+
+
+
+// acceso condicional a un objeto
+
+
+const usuario = {
+nombre:"jose",
+cursos:{
+css:"aprobado",
+js:"aprobado"
+},
+}
+
+console.log(usuario?.cursos || "la propiedad no existe")
+console.log(usuario?.cursos.css || "la propiedad no existe")
+
+
